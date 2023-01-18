528170266eddc2f6ddd33f2cca8ec5d0e63738e2
Karla Fajardo <karlafardo07@gmail.com>
2023-01-18 17:29:24 -0500

    homewrok2

---
+++ 02 - Variables y Tipos de Datos/## solucion.py
@@ -0,0 +1,68 @@
+## solucion
+import math
+entero=13
+print(entero)
+
+constante=8.5
+print(type(constante))
+
+print (type(entero))
+
+Varnombre='Karla'
+
+varcom= 1 + 1j
+
+print(type(varcom))
+
+
+phi= round(math.pi,4)
+print(phi)
+
+varst='True'
+varbol=True
+## no, la primera es un string porque esta en apostrofes y la segunda un dato booleano
+
+print(type(varst))
+print(type(varbol))
+
+suma=10+20.1
+
+sumacomp= 10+1j + 20+3j
+
+sumarcom= 10 + 20+1j
+
+3*3
+
+print(2**8)
+
+division=27/4
+print(division)
+
+pent=27//4
+print(pent)
+
+resto=27%4
+print(resto)
+
+print(resto+ (pent*4))
+
+varal='william'
+varal1='karla'
+amor=varal+ ' + '+varal1
+print(amor)
+
+print('2'==2)
+
+print(int('2')==2)
+
+##porque se uso coma en vez de punto y el lenguaje no lo reconoce
+
+x=3
+x-=1
+
+print(1<<2) 
+
+
+##no esta permitido porque los datos no son del mismo tipo, no daria el mismo resultado porque si se pone como entero nos daria la suma de los numeros pero si se ponen como string concatenaria los numeros
+
+'karla'*3
